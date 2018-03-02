20fe6743d68fb02391c14e6d1fe612db656e829b
冉慕飞 <362134111@qq.com>
2018-03-02 17:46:13 +0800

    init code

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+</template>
+    <ul>
+        <li></li>
+    </ul>
+</template>
+
+<script type>
+export default{
+    name:'header',
+    data:function(){
+        return {
+            nav:['Home','About','Content'],
+
+        }
+    }
+}
+
+</script>
+
+<style>
+    
+</style>
