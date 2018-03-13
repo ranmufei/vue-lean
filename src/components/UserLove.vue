46e3498700e867c6bedd902653a84133ea30242b
冉慕飞 <362134111@qq.com>
2018-03-13 17:34:34 +0800

    add

---
+++ src/components/UserLove.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-checkbox-group 
+    v-model="checkedCities1"
+    :min="1"
+    :max="2">
+    <el-checkbox v-for="city in cities" :label="city" :key="city">{{city}}</el-checkbox>
+  </el-checkbox-group>
+</template>
+<script>
+  const cityOptions = ['上海', '北京', '广州', '深圳'];
+  export default {
+    data() {
+      return {
+        checkedCities1: ['上海', '北京'],
+        cities: cityOptions
+      };
+    }
+  };
+</script>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,36 @@
+<template>
+    <transition
+  name="bounce"
+  enter-active-class="bounceInLeft"
+  leave-active-class="bounceOutRight"
+>
+    <div>
+        <el-progress type="circle" :percentage="0"></el-progress>
+        <el-progress type="circle" :percentage="25"></el-progress>
+        <el-progress type="circle" :percentage="100" status="success"></el-progress>
+        <el-progress type="circle" :percentage="50" status="exception"></el-progress>
+    
+        <p>姓名：{{$route.params.name}}</p>
+        <p>年龄：{{$route.params.old}}</p>
+        <p>籍贯：{{$route.params.from}}</p>
+        
+
+    </div>
+    </transition>
+</template>
+
+<script>
+export default  {
+    name:'Header',
+    data () {
+        return {
+         name:'fe'
+                   }
+    }
+}
+
+</script>
+
+<style>
+    
+</style>
